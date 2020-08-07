7fb342c578198012706707ffacdfb66c548aac63
Pintobeanz <49594005+Pintobeanz@users.noreply.github.com>
2020-08-07 13:25:16 -0400

    Add files via upload

---
+++ assets/Data/myData.ts
@@ -0,0 +1,9 @@
+import {Pintoal} from '../../app/pintoal';
+
+export const MYINFO: Pintoal = {
+  number: 911360378,
+  name: 'Alexander Pinto',
+  login: 'Pintoal',
+  campus: 'Davis Campus',
+  assignment: 'Assignment 4'
+};
